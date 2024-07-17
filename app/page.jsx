707e9591b9bef41dc2c8b215712d948f1c92db02
Pojj <williambsong@gmail.com>
2024-07-17 12:13:11 -0700

    Create homepage

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <section className="w-full flex justify-center items-center flex-col">
+      <h1 className="text-center mb-6 mt-20 px-4 font-extrabold text-7xl sm:text-8xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-yellow-400 from-orange-600">
+          Macro Manager
+        </span>
+      </h1>
+      <p className="text-center md:w-1/2 text-lg font-normal text-gray-500 lg:text-xl px-5">
+        Track macrooos and recipes and gainz
+      </p>
+
+      <p> Newest recipe additions</p>
+    </section>
+  );
+}
