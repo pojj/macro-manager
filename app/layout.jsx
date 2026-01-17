eb2c5c0cb08b5e9d705420b0ced63adec764d4cf
William Song <williambsong@gmail.com>
2026-01-16 17:12:45 -0700

    Create homepage

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import { Inter } from "next/font/google";
+import "@styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Macro Manager",
+  description: "Track/plan your meals/macros",
+  icons: {
+    icon: "/assets/favicon.ico",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className + " dark:bg-white dark:text-black"}>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
